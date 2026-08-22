61fdebfe0cbcb4bac581fedbe318eee39cd6a083
RexP10r <jlimonadnui@gmail.com>
2026-08-22 14:42:05 +0300

    feat(problems): lc643

---
+++ problems/easy/lc643.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMaxAverage(self, nums: list[int], k: int) -> float:
+        sum = 0
+        for i in range(k):
+            sum += nums[i]
+        max_sum = sum
+        for i in range(k, len(nums)):
+            sum = sum - nums[i - k] + nums[i]
+            max_sum = max(max_sum, sum)
+        return max_sum / k
