ac65b626e6718615ca9ebfd823b319d576b40a4c
RexP10r <jlimonadnui@gmail.com>
2026-09-03 16:52:30 +0300

    feat(problems): lc649

---
+++ problems/medium/lc649.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        radiant = deque()
+        dire = deque()
+
+        for i, char in enumerate(senate):
+            if char == "R":
+                radiant.append(i)
+            else:
+                dire.append(i)
+
+        while radiant and dire:
+            r_idx = radiant.popleft()
+            d_idx = dire.popleft()
+
+            if r_idx < d_idx:
+                radiant.append(r_idx + n)
+            else:
+                dire.append(d_idx + n)
+
+        return "Radiant" if radiant else "Dire"
